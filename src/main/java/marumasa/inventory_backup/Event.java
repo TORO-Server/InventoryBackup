b845d9a95005768d056329c5d5ccd7d69ed67e6e
malken21 <Malken_Marumasa@outlook.jp>
2023-11-10 20:41:34 +0900

    Update Event.java

--- src/main/java/marumasa/inventory_backup/Event.java
+++ src/main/java/marumasa/inventory_backup/Event.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -54,9 +55,9 @@ public class Event implements Listener {
     }
 
     @EventHandler
-    public void onInventory(InventoryClickEvent event) {
+    public void onInventory(InventoryOpenEvent event) {
         // プレイヤー取得
-        final HumanEntity player = event.getWhoClicked();
+        final HumanEntity player = event.getPlayer();
         // プレイヤーからインベントリ取得
         final PlayerInventory inventory = player.getInventory();
         // インベントリからアイテムの配列を取得
