5f849cce09efeb4a13045ac37d4870542d07f90a
malken21 <Malken_Marumasa@outlook.jp>
2023-11-10 15:39:50 +0900

    setup

---
+++ src/main/java/marumasa/inventorybackup/Minecraft.java
@@ -0,0 +1,17 @@
+package marumasa.inventorybackup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
