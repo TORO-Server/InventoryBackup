9442871bd881df9db16ea443e16b5907a0d5e57a
malken21 <Malken_Marumasa@outlook.jp>
2023-11-10 16:34:11 +0900

    Update Event.java

--- src/main/java/marumasa/inventorybackup/Event.java
+++ src/main/java/marumasa/inventorybackup/Event.java
@@ -1,10 +1,23 @@
 package marumasa.inventorybackup;
 
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static marumasa.inventorybackup.Utils.cloneInventory;
 
 public class Event implements Listener {
+
+    private static final Map<HumanEntity, ItemStack[]> InventoryBackup = new HashMap<>();
+
 
     private final Config cfg;
     private final Minecraft mc;
@@ -15,6 +28,23 @@ public class Event implements Listener {
     }
 
     @EventHandler
-    public void onInventory(InventoryEvent event) {
+    public void onInventory(InventoryCloseEvent event) {
+
+        final HumanEntity player = event.getPlayer();
+        final PlayerInventory inventory = player.getInventory();
+
+        final ItemStack[] itemStacks = inventory.getContents();
+
+        for (ItemStack i : itemStacks)
+            player.sendMessage(i.toString());
+
+
+        // バックアップに追加
+        InventoryBackup.put(player, inventory.getContents().clone());
+
+
+        mc.getServer().broadcastMessage(player.getName());
+
+        inventory.setContents(InventoryBackup.get(player));
     }
 }
